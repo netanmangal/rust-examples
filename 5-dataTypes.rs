c9f6468cf7631a7e65ad294fe7bb86bbda48d9d5
netanmangal <imnetanmangal@gmail.com>
2021-12-29 22:55:06 +0530

    6-if-else

---
+++ 5-dataTypes.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let _unsigned_integer: u64 = 5;
+    let _signed_integer_32_bit: i32 = -43;
+    let _signed_integer_64_bit: i64 = -28;
+    let _float_64: f64 = -8.74;
+    let _string_variable = "netizaq";
+    let _boolean_variable = true;
+}
---
+++ 6-if-else.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let rohan_likes = "Apple";
+
+    // if (rohan_likes == "Apple")   <--  Warning
+    if rohan_likes == "Apple" {
+        println!("Give him Apple!");
+    } else if rohan_likes == "Mango" {
+        println!("Give him Mango!");
+    } else {
+        println!("Rohan neither wanta Apple nor Mango.");
+    }
+}
