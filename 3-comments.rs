889863870a3bbbdf21e6fbe202a188d419237fbf
netanmangal <imnetanmangal@gmail.com>
2021-12-29 22:20:25 +0530

    3-comments

---
+++ 3-comments.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // This is a single line comment.
+    // This is the print line macro printing Hello netizaqians.
+    println!("Hello netizaqians");
+
+    /*
+    This is a multiline comment.
+    This is printing "Happy coding 😀 !!!" to the console
+    */
+    println!("Happy coding 😀 !!!");
+}
