fc54258348829bc67e8e00a86b7d3bcaa4de0c94
netanmangal <imnetanmangal@gmail.com>
2021-12-29 22:56:13 +0530

    6-if-else-updated-warning

--- 6-if-else.rs
+++ 6-if-else.rs
@@ -1,8 +1,8 @@
 fn main() {
     let rohan_likes = "Apple";
 
-    // if (rohan_likes == "Apple")   <--  Warning
-    if rohan_likes == "Apple" {
+    // if (rohan_likes == "Apple")   <--  Warning #[warn(unused_parens)]
+    if (rohan_likes == "Apple") {
         println!("Give him Apple!");
     } else if rohan_likes == "Mango" {
         println!("Give him Mango!");
