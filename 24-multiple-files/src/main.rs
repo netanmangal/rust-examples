07e7359f05929096ec89057dc1f0951390256a9e
netanmangal <imnetanmangal@gmail.com>
2022-03-18 22:34:33 +0530

    Update: 24-multiple-files

--- 24-multiple-files/src/main.rs
+++ 24-multiple-files/src/main.rs
@@ -4,9 +4,26 @@
 //     table::print_table(12);
 // }
 
-mod table;
-use crate::table::print_table;
+
+
+// ---- Way 2 ----
+// mod table;
+// use crate::table::print_table;
+
+// fn main() {
+//     print_table(12);
+// }
+
+
+// ---- Way 3 ----
+mod table {
+    pub fn print_table (num: u32) {
+        for i in 1..11 {
+            println!("{} * {} = {}", num, i, num * i);
+        }
+    }
+}
 
 fn main() {
-    print_table(12);
+    table::print_table(15);
 }
