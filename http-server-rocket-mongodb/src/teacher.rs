50b27475aa96284463b2fb752dc60c1c880066ab
netanmangal <imnetanmangal@gmail.com>
2022-12-17 21:14:27 +0530

    Update: format teacher get

--- http-server-rocket-mongodb/src/teacher.rs
+++ http-server-rocket-mongodb/src/teacher.rs
@@ -5,11 +5,13 @@ use rocket::*;
 pub fn get_teacher(id: u8) -> String {
     let mut teacher: &TeacherInfo = &TeacherInfo::new();
 
-    unsafe { for (_, o) in TEACHERS.iter().enumerate() {
-        if id == o.id {
-            teacher = o;
+    unsafe {
+        for (_, o) in TEACHERS.iter().enumerate() {
+            if id == o.id {
+                teacher = o;
+            }
         }
-    } }
+    }
 
     format!("{:#?}", teacher)
 }
