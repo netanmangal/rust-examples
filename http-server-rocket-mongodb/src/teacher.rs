19778fd2e6a0803a7d079f250807cdf4f445bb64
netanmangal <imnetanmangal@gmail.com>
2022-12-17 16:32:04 +0530

    Add: GET for teacher using id

--- http-server-rocket-mongodb/src/teacher.rs
+++ http-server-rocket-mongodb/src/teacher.rs
@@ -1,6 +1,15 @@
+use crate::state::*;
 use rocket::*;
 
 #[get("/?<id>")]
-pub fn get_teacher(id: &str) -> String {
-    format!("Hello.\nFetching details of teacher - id {}", id)
+pub fn get_teacher(id: u8) -> String {
+    let mut teacher: &TeacherInfo = &TeacherInfo::new();
+
+    unsafe { for (_, o) in TEACHERS.iter().enumerate() {
+        if id == o.id {
+            teacher = o;
+        }
+    } }
+
+    format!("{:#?}", teacher)
 }
